689d99b07926a9fc97e04eac9e7ea604cb7b7187
Jason Brunch <jasonbunce2@gmail.com>
2024-04-08 19:02:36 -0700

    Added some styling to the right control panel

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { initializeSchedule, updateActivity } from "@/managers/planManager";
-import { colorMapping } from "@/managers/styleManager";
+
 
 
 
@@ -17,12 +17,18 @@ function Home() {
         // Update the daySchedule state with the new schedule
         setDaySchedule(updatedSchedule);
     };
+    const colorMapping = {
+        green: "bg-green-500",
+        red: "bg-red-500",
+        blue: "bg-blue-500",
+        // Add more mappings as needed
+    };
 
     
         return (
-            <>
+            <div className="flex">
                 {/* Schedule Container */}
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center w-2/3">
                     {Array.from({ length: 24 }, (_, hour) => (
                         <div key={hour} className="flex w-full border-b border-gray-300">
                             {/* Hour Display */}
@@ -48,38 +54,51 @@ function Home() {
                     ))}
                 </div>
                 {/* Inputs and Button Container */}
-            <div className="my-4 p-4">
-                <input
-                    type="time"
-                    value={startTime}
-                    onChange={e => setStartTime(e.target.value)}
-                    className="mr-2"
-                />
-                <input
-                    type="time"
-                    value={endTime}
-                    onChange={e => setEndTime(e.target.value)}
-                    className="mr-2"
-                />
-                <input
-                    type="text"
-                    value={activityTitle}
-                    placeholder="Activity Title"
-                    onChange={e => setActivityTitle(e.target.value)}
-                    className="mr-2"
-                />
-                <input
-                    type="text"
-                    value={activityColor}
-                    placeholder="Color"
-                    onChange={e => setActivityColor(e.target.value)}
-                    className="mr-2"
-                />
-                <button onClick={handleAddActivity} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Add Activity
-                </button>
-            </div>
-        </>
+                <div className="w-1/3 bg-orange-100">
+        
+                <div className="w-full h-40 bg-customBlue ">
+                    
+                </div >
+
+                <div className="bg-gray-100 relative overflow-hidden h-full" >
+                    <div className="absolute top-0 left-0 w-full h-[100px]" style={{ backgroundImage: "url('/wave.svg')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}></div>
+                    <div className="mt-20">
+                                <input
+                                    type="time"
+                                    value={startTime}
+                                    onChange={e => setStartTime(e.target.value)}
+                                    className="mr-2"
+                                />
+                                <input
+                                    type="time"
+                                    value={endTime}
+                                    onChange={e => setEndTime(e.target.value)}
+                                    className="mr-2"
+                                />
+                                <input
+                                    type="text"
+                                    value={activityTitle}
+                                    placeholder="Activity Title"
+                                    onChange={e => setActivityTitle(e.target.value)}
+                                    className="mr-2"
+                                />
+                                {/* Color Selection Dropdown */}
+                                <select
+                                    value={activityColor}
+                                    onChange={e => setActivityColor(e.target.value)}
+                                    className="mr-2"
+                                >
+                                    {Object.keys(colorMapping).map(color => (
+                                        <option key={color} value={color}>{color.charAt(0).toUpperCase() + color.slice(1)}</option>
+                                    ))}
+                                </select>
+                                <button onClick={handleAddActivity} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                    Add Activity
+                                </button>
+                                </div>
+                                </div>
+                        </div>
+        </div>
     );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        customBlue: '#000b76', 
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
