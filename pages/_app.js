4d0ac92cb0af9a5ac05cdc2054937b7ded200b9f
Jason Brunch <jasonbunce2@gmail.com>
2024-05-10 15:09:42 -0700

    Added some SEO to the _app.js and a favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,22 @@
 import React, { useEffect } from "react";
 import "../globals.css";
 import { UserProvider, useUser } from "@/context/userContext";
+import Head from "next/head"; 
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
+      <Head>
+        <title>Day Forge - Optimize Your Day</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Day Forge helps you organize your day with advanced day planning tools, calorie tracking, and customizable task lists. Achieve your daily goals effortlessly!" />
+        <link rel="icon" href="/favicon.ico" /> 
+        <meta property="og:title" content="Day Forge - Optimize Your Day" />
+        <meta property="og:description" content="Discover the ultimate tool for day planning, calorie tracking, and list making to streamline your daily activities and health management." />
+        <meta property="og:image" content="/dayforge.jpg" />
+        <meta property="og:url" content="https://www.yourdomain.com" />
+        <meta property="og:type" content="website" />
+      </Head>
       <ComponentWithUser Component={Component} {...pageProps} />
     </UserProvider>
   );
